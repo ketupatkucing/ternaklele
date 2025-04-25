da8bc1992dcda0b3d631e1c2a0d72eca8f601830
ketupatkucing <gammertag31@gmail.com>
2025-04-25 15:52:08 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,47 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        'index.html',
-        'fish.js',
-        'mancing.js',
-        'pancing.png',
-        'strike.png',
-        'bait.png',
-        'coin.png',
-        'lele.png',
-        'makananpelet.png',
-        'talas.png',
-        'pelet.png',
-        'orang.png'
-        
-      ]);
+
+
+const CACHE_NAME = 'ternaklele-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/ternaklele/',
+  '/ternaklele/index.html',
+  '/ternaklele/fish.js',
+  '/ternaklele/mancing.js',
+  '/ternaklele/pancing.png',
+  '/ternaklele/strike.png',
+  '/ternaklele/bait.png',
+  '/ternaklele/coin.png',
+  '/ternaklele/lele.png',
+  '/ternaklele/makananpelet.png',
+  '/ternaklele/talas.png',
+  '/ternaklele/pelet.png',
+  '/ternaklele/orang.png'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME)
+                  .map((name) => caches.delete(name))
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedRes) => {
+      return cachedRes || fetch(event.request);
     })
   );
 });
